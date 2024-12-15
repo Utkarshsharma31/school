1218bb7491a27e1a5431db986fcdf6c46fc18ca2
utkarsh sharma <utkarshsharmaalua@gmail.com>
2024-12-15 14:57:03 +0530

    header commit

---
+++ src/app/component/Student/student.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function student() {
+  return (
+<div class="container">
+      <div class="card">
+        <div class="img-div">
+          <img src="boy1.jpg" alt="images"/>
+        </div>
+        <h1>003</h1>
+        <h2>ABUJAR</h2>
+        <div class="card-container">
+          <div class="circle1">
+            <div class="contant1">
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+          </div>
+          <div class="circle2">
+            <div class="contant2">
+              <i class="fa-solid fa-pen"></i>
+            </div>
+          </div>
+          <div class="circle3">
+            <div class="contant3">
+              <i class="fa-solid fa-trash"></i>
+            </div>
+          </div>
+        </div>
+        <span></span>
+        <span></span>
+
+      </div>
+    </div>
+  )
+}
+
+export default student
